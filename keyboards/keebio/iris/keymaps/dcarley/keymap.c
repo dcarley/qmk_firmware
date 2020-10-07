df68d5475ebd7a6af66dd5ccb3ebe4957d42a78b
Dan Carley <dan.carley@gmail.com>
2020-10-07 16:27:14 +0100

    iris/dcarley: Fix backslash
    By detecting as ISO and moving backtick to thumb. This means that
    backtick is still accessible on the first layer with a single press.

--- keyboards/keebio/iris/keymaps/dcarley/keymap.c
+++ keyboards/keebio/iris/keymaps/dcarley/keymap.c
@@ -8,17 +8,30 @@ extern keymap_config_t keymap_config;
 
 // hold ctrl, tap enter.
 #define CTL_ENT MT(MOD_LCTL, KC_ENT)
+// hold gui, tap backtick
+// GRAVE_ESC backtick doesn't work on Mac:
+// https://github.com/qmk/qmk_firmware/issues/6006
+#define GUI_GRV MT(MOD_LGUI, KC_GRV)
 // delete word.
 #define JC_BSPC LALT(KC_BSPC)
 // jump words.
 #define JC_LEFT LALT(KC_LEFT)
 #define JC_RGHT LALT(KC_RGHT)
 
+/*
+Mappings when used on Mac with British PC and ISO
+
+         normal  shifted
+KC_NUBS  \       |
+KC_NUHS  #       ~
+KC_GRV   `       ¬
+*/
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -26,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT,          _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NUHS,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    MO(NAV), KC_LGUI, CTL_ENT,                   KC_SPC,  MO(SYM), _______
+                                    MO(NAV), GUI_GRV, CTL_ENT,                   KC_SPC,  MO(SYM), _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
